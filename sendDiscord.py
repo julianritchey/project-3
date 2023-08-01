618979ba6f7de52ccbe867aa52abdb5adf8ca4f9
Wanlin Li <wanlinli2018@gmail.com>
2023-07-31 22:52:39 -0400

    send notification to Discord community

---
+++ sendDiscord.py
@@ -0,0 +1,17 @@
+# reference: https://10mohi6.medium.com/super-easy-python-discord-notifications-api-and-webhook-9c2d85ffced9
+import requests
+import os
+from dotenv import load_dotenv
+from discordwebhook import Discord
+load_dotenv()
+
+
+def send_notification(url):
+    # send message for our club and community
+    discord = Discord(url=url)
+    discord.post(content="Hello, welcome to Investor's Dream.")
+
+
+if __name__ == '__main__':
+    url = os.getenv("webHook_url")
+    send_notification(url)
